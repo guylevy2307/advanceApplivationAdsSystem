cc8285660a5441afcaf63d6ce4ea97c7ca3fe88f
DESKTOP-NFKEKDB\guylevy <guylevy2307@gmail.com>
2022-08-20 01:11:28 +0300

    change the db connection

--- server/index.js
+++ server/index.js
@@ -7,27 +7,28 @@ const UserRouter = require("./Routes/userRoutes");
 const PostRouter = require("./Routes/postRoutes");
 const CommentRouter = require("./Routes/commentRoutes");
 const CMSFunc = require("./Utils/most-popular-first-names");
-const {conversationRouter} = require("./Routes/conversationRoutes");
-const {messageRouter} = require("./Routes/messageRoutes");
+const { conversationRouter } = require("./Routes/conversationRoutes");
+const { messageRouter } = require("./Routes/messageRoutes");
 // const User = require("./Controllers/userController");
 
 
 const port = process.env.port || 5000;
 const db = 'mongodb+srv://admin:1234@lulilanddb.j4tppp6.mongodb.net/?retryWrites=true&w=majority'
+//const db = process.env.MONGO_URI; 
 dotenv.config();
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const corsOptions ={
-    origin:'http://localhost:3000',
-    credentials:true,
-    optionSuccessStatus:200
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
 }
 app.use(cors(corsOptions));
 
-app.use(express.json({ extended:true }));
-app.use(express.urlencoded({ extended:true }));
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 // app.use(cors());
 app.use('/users', UserRouter);
 app.use('/posts', PostRouter);
@@ -35,7 +36,7 @@ app.use('/comments', CommentRouter);
 app.use('/conversation', conversationRouter)
 app.use('/message', messageRouter)
 
-app.get('/',(req, res) => {res.send('Im alive');});
-mongoose.connect(db,{ useUnifiedTopology: true, useNewUrlParser: true}).then(()=>app.listen(port, ()=>{
+app.get('/', (req, res) => { res.send('Im alive'); });
+mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
-})).catch((err)=>console.log('dont succeed to connect'));
+})).catch((err) => console.log('dont succeed to connect'));
