9de6c8f96af4c3b18f5246608b503d77e591da0b
DESKTOP-NFKEKDB\guylevy <guylevy2307@gmail.com>
2022-08-20 09:54:21 +0300

    update login page

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,19 +1,20 @@
-import React, {createRef, useRef, useState} from 'react';
+import React, { createRef, useRef, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import validator from "validator/es";
-import {getUserByEmail, login, signUp} from "../../services/UserService";
-import {useNavigate} from "react-router-dom";
+import { getUserByEmail, login, signUp } from "../../services/UserService";
+import { useNavigate } from "react-router-dom";
 import "./loginForm.css"
-import {notification} from "antd";
+import { notification } from "antd";
 import 'antd/dist/antd.css';
+import logoImage from '../Topbar/adsystem.png';
 
 export default function FormDialog() {
     const [open, setOpen] = useState(false)
     const [errors, setErrors] = useState({})
-    const [successLogIn,setSuccessLogIn] = useState("PENDING")
-    const [validEmail,setValidEmail] = useState(true)
-    const [validPassword,setValidPassword] = useState(true)
+    const [successLogIn, setSuccessLogIn] = useState("PENDING")
+    const [validEmail, setValidEmail] = useState(true)
+    const [validPassword, setValidPassword] = useState(true)
     const emailRef = useRef("")
     const passwordRef = useRef("")
     const navigate = useNavigate();
@@ -22,30 +23,29 @@ export default function FormDialog() {
     const validateError = () => {
         const email = emailRef.current.value
         const password = passwordRef.current.value
-        console.log({email,password});
+        console.log({ email, password });
         const currentErrors = errors
         setValidEmail(validator.isEmail(email))
-        if(!validEmail)
+        if (!validEmail)
             currentErrors.email = "Please provide a correct email address."
-        setValidPassword(password.length!==0)
-        if(!validPassword)
+        setValidPassword(password.length !== 0)
+        if (!validPassword)
             currentErrors.password = "Password can't be empty."
         setErrors(currentErrors)
     }
 
-    const signIn = async ()=> {
-        const credentials = {email:emailRef.current.value,password: passwordRef.current.value}
+    const signIn = async () => {
+        const credentials = { email: emailRef.current.value, password: passwordRef.current.value }
         var res = true
-        try{
+        try {
             await login(credentials)
-        }catch{
-            res=false
+        } catch {
+            res = false
         }
-        if(res)
-        {
+        if (res) {
             setSuccessLogIn("SUCCESS")
             const user = await getUserByEmail(emailRef.current.value);
-            localStorage.setItem("user",JSON.stringify(user))
+            localStorage.setItem("user", JSON.stringify(user))
             //localStorage.setItem("password",passwordRef.current.value)
             navigate("/")
             handleClose()
@@ -72,43 +72,43 @@ export default function FormDialog() {
 
     return (
         <>
-        <h1 className="title">Luliland</h1>
-        <div className="loginBox">
-            <TextField
-                error={!validEmail}
-                inputRef={emailRef}
-                autoFocus
-                margin="dense"
-                label="Email Address"
-                type="email"
-                onChange={validateError}
-                helperText={!validEmail ? errors.email : ""}
-                fullWidth
-            />
-            <br />
-            <TextField
-                error={!validPassword}
-                inputRef={passwordRef}
-                autoFocus
-                margin="dense"
-                label="Password"
-                type="password"
-                onChange={validateError}
-                helperText={!validPassword ? errors.password : ""}
-                fullWidth
-            />
-            {successLogIn==="FAILED"
-                // <label className={"errorMessage"}>Email or password is wrong</label>
-            }
-            <Button style={{paddingTop: 20}} onClick={signIn} color="primary" disabled={!validEmail || !validPassword}>
-                Sign In
-            </Button>
-            <Button style={{paddingTop: 20}} onClick={(e) => {
-                navigate("/register")
-            }} color="primary">
-                Sign Up
-            </Button>
-        </div>
+            <img src={logoImage} alt="logo" />
+            <div className="loginBox">
+                <TextField
+                    error={!validEmail}
+                    inputRef={emailRef}
+                    autoFocus
+                    margin="dense"
+                    label="Email Address"
+                    type="email"
+                    onChange={validateError}
+                    helperText={!validEmail ? errors.email : ""}
+                    fullWidth
+                />
+                <br />
+                <TextField
+                    error={!validPassword}
+                    inputRef={passwordRef}
+                    autoFocus
+                    margin="dense"
+                    label="Password"
+                    type="password"
+                    onChange={validateError}
+                    helperText={!validPassword ? errors.password : ""}
+                    fullWidth
+                />
+                {successLogIn === "FAILED"
+                    // <label className={"errorMessage"}>Email or password is wrong</label>
+                }
+                <Button style={{ paddingTop: 20 }} onClick={signIn} color="primary" disabled={!validEmail || !validPassword}>
+                    Sign In
+                </Button>
+                <Button style={{ paddingTop: 20 }} onClick={(e) => {
+                    navigate("/register")
+                }} color="primary">
+                    Sign Up
+                </Button>
+            </div>
         </>
     );
 }
